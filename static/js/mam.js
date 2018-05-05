5a465721c75a46d10dd2771f34075a808f173088
marton bognar <martonbognar@sch.bme.hu>
2018-05-05 04:48:50 +0200

    Save COM device

--- static/js/mam.js
+++ static/js/mam.js
@@ -37,8 +37,35 @@ function mamParse(data) {
     }
 }
 
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
 var initButtonMam = document.getElementById('init-mam');
+var comCookie = getCookie('com');
+if (comCookie !== null) {
+    var comInput = document.getElementById('mam-com');
+    comInput.value = comCookie;
+}
 initButtonMam.addEventListener('click', function (event) {
     var com = document.getElementById('mam-com');
+    setCookie('com', com.value, 30);
     socket.send(JSON.stringify({'action': 'init', 'target': 'mam', 'com': com.value}));
 });
