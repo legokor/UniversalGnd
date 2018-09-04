4caf4a90dd19ea3080e3010ba1fd43d91a6a3886
Dániel Móna <mona.daniel97@gmail.com>
2018-09-04 22:53:26 +0200

    Separate ground control perspectives

--- tracker/urls.py
+++ tracker/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path('update-item/<int:pk>', views.update_task, name='toggle'),
     path('checklist', views.admin, name='admin'),
+    path('upra/flight', views.upra_flight, name='upra_flight'),
+    path('upra/com', views.upra_communication, name='upra_communication'),
+    path('upra/telemetry', views.upra_telemetry, name='upra_telemetry'),
     path('', views.index, name='index'),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -11,12 +11,25 @@ from .consumers import broadcast
 
 
 def index(request):
+    return render(request, 'tracker/index.html')
+
+def mam(request):
     context = {
-        'pagetitle': 'UPRA',
+        'pagetitle': 'MaM',
         'maintabs': [
-            {'id': 'init', 'title': 'Init', 'template': 'tracker/panels/init.html'},
-            {'id': 'tasks', 'title': 'Tasks', 'template': 'tracker/panels/tasks.html'},
+            {'id': 'init', 'title': 'Init', 'template': 'tracker/panels/init-mam.html'},
             {'id': 'mam-controls', 'title': 'MaM controls', 'template': 'tracker/panels/mam-controls.html'},
+            {'id': 'debug-console', 'title': 'Debug Console', 'template': 'tracker/panels/debug-console.html'}
+        ],
+        'showmap': True,
+        'bottom_panel_template': 'tracker/panels/telemetry-graphs.html'
+    }
+    return render(request, 'tracker/main.html', context)
+
+def quadcopter(request):
+    context = {
+        'pagetitle': 'QuadCopter',
+        'maintabs': [
             {'id': 'qc-controls', 'title': 'QC controls', 'template': 'tracker/panels/qc-controls.html'},
             {'id': 'program-console', 'title': 'Program Console', 'template': 'tracker/panels/program-console.html'},
             {'id': 'debug-console', 'title': 'Debug Console', 'template': 'tracker/panels/debug-console.html'}
@@ -24,7 +37,46 @@ def index(request):
         'showmap': True,
         'bottom_panel_template': 'tracker/panels/telemetry-graphs.html'
     }
-    return render(request, 'tracker/index.html', context)
+    return render(request, 'tracker/main.html', context)
+
+def upra_flight(request):
+    context = {
+        'pagetitle': 'Flight | UPRA',
+        'maintabs': [
+            {'id': 'init', 'title': 'Init', 'template': 'tracker/panels/init-upra.html'},
+            {'id': 'tasks', 'title': 'Tasks', 'template': 'tracker/panels/tasks.html'},
+            {'id': 'program-console', 'title': 'Program Console', 'template': 'tracker/panels/program-console.html'},
+            {'id': 'debug-console', 'title': 'Debug Console', 'template': 'tracker/panels/debug-console.html'}
+        ],
+        'showmap': True,
+        'bottom_panel_template': 'tracker/panels/telemetry-graphs.html'
+    }
+    return render(request, 'tracker/main.html', context)
+
+def upra_communication(request):
+    context = {
+        'pagetitle': 'Communication | UPRA',
+        'maintabs': [
+            {'id': 'init', 'title': 'Init', 'template': 'tracker/panels/init-upra.html'},
+            {'id': 'program-console', 'title': 'Program Console', 'template': 'tracker/panels/program-console.html'},
+            {'id': 'debug-console', 'title': 'Debug Console', 'template': 'tracker/panels/debug-console.html'}
+        ],
+        'showmap': False
+    }
+    return render(request, 'tracker/main.html', context)
+
+def upra_telemetry(request):
+    context = {
+        'pagetitle': 'Telemetry | UPRA',
+        'maintabs': [
+            {'id': 'init', 'title': 'Init', 'template': 'tracker/panels/init-upra.html'},
+            {'id': 'program-console', 'title': 'Program Console', 'template': 'tracker/panels/program-console.html'},
+            {'id': 'debug-console', 'title': 'Debug Console', 'template': 'tracker/panels/debug-console.html'}
+        ],
+        'showmap': True,
+        'bottom_panel_template': 'tracker/panels/telemetry-graphs.html'
+    }
+    return render(request, 'tracker/main.html', context)
 
 
 @login_required(login_url='/admin/login/')
