316d2e70e75d2c60032519506fa0a4b8c38021a8
Dániel Móna <mona.daniel97@gmail.com>
2018-09-04 23:21:32 +0200

    Add missing URLs

--- tracker/urls.py
+++ tracker/urls.py
@@ -4,9 +4,11 @@ from . import views
 
 urlpatterns = [
     path('update-item/<int:pk>', views.update_task, name='toggle'),
-    path('checklist', views.admin, name='admin'),
+    path('upra/checklist', views.admin, name='admin'),
     path('upra/flight', views.upra_flight, name='upra_flight'),
     path('upra/com', views.upra_communication, name='upra_communication'),
     path('upra/telemetry', views.upra_telemetry, name='upra_telemetry'),
+    path('mam/mam-2018', views.mam, name='mam_2018'),
+    path('qc/main', views.quadcopter, name='qc_main'),
     path('', views.index, name='index'),
 ]
