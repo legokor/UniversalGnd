85b845aa64840ef860b1783d3c5613064729a8aa
Dániel Móna <mona.daniel97@gmail.com>
2019-09-04 17:15:05 +0200

    Start implementing editable value displays as a component

--- static/js/visualizer.js
+++ static/js/visualizer.js
@@ -51,52 +51,6 @@ function updateControlValue(name, value) {
     text.value = value;
 }
 
-
-class EditableValueDisplay {
-
-    constructor(cont, keyName, packetType, publishPacketType) {
-        this.keyName = keyName;
-        this.packetType = packetType;
-        this.publishPacketType = publishPacketType;
-
-        this.valueHolder = cont.getElementsByClassName('value-holder')[0];
-        this.updateStatusHolder = cont.getElementsByClassName('update-status-holder')[0];
-
-        this.valueHolder.addEventListener('keydown', (event) => {
-            if (event.keyCode === 13) { // Enter
-                this.publishValue();
-            }
-        });
-
-        dispatcher.subscribe(packetType, (packet) => { this.updateValue(packet); });
-    }
-
-    updateValue(packet) {
-        this.valueHolder.value = packet[this.keyName];
-    }
-
-    publishValue() {
-        let packet = {};
-        packet['type'] = this.publishPacketType;
-        packet[this.keyName] = this.valueHolder.value;
-
-        console.log(JSON.stringify(packet));
-        socket.send(JSON.stringify(packet));
-
-        if (this.updateStatusHolder != null) {
-            this.updateStatusHolder.classList.add('saving');
-            this.updateStatusHolder.classList.remove('saved');
-
-            window.setTimeout(2500, () => {
-                this.updateStatusHolder.classList.add('saved');
-                this.updateStatusHolder.classList.remove('saving');
-            });
-        }
-    }
-}
-
-let editableValueDisplays = [];
-
 document.addEventListener("keydown", function (event) {
     event = event || window.event;
 
@@ -122,22 +76,6 @@ window.addEventListener("load", function () {
                 let value = this.value;
                 // updateControlValue(field, value);
             }, input);
-        }
-    }
-
-    // Hook up EditableValueDisplays
-    let editableValConts = document.getElementsByClassName('value-display-editable');
-    for (let i = 0; i < editableValConts.length;  i++) {
-        if (editableValConts[i].hasAttribute('data-packet-type') && editableValConts[i].hasAttribute('data-publish-packet-type')) {
-            let packetType = editableValConts[i].getAttribute('data-packet-type');
-            let publishPacketType = editableValConts[i].getAttribute('data-publish-packet-type');
-
-            let keyName = 'value';
-            if (editableValConts[i].hasAttribute('data-key-name')) {
-                keyName = editableValConts[i].getAttribute('data-key-name');
-            }
-
-            editableValueDisplays.push(new EditableValueDisplay(editableValConts[i], keyName, packetType, publishPacketType));
         }
     }
 });
