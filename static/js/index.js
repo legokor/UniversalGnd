aa6d3f1f71a1f5546ae0be5ffd110abdb57918f9
Dániel Móna <mona.daniel97@gmail.com>
2019-09-27 17:20:23 +0200

    Try getting the map to load

--- static/js/index.js
+++ static/js/index.js
@@ -4,6 +4,11 @@ import ReactDOM from "react-dom"
 import { UtcClock } from "./UtcClock";
 import { GndApp } from "./GndApp";
 
+import * as map from "./map"
+
+// Make this global so the GMaps API can see it.
+windown.initMap = map.initMap;
+
 ReactDOM.render(
     <UtcClock/>,
     document.getElementById("header-time-wrapper")
